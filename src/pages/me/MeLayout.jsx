949fd166ef043f2307e99c3626907aee5ff73e87
Kendercuba <kendercuba92@gmail.com>
2025-06-05 21:18:15 -0400

    🧼 Fix: deploy solo de dist/

--- src/pages/me/MeLayout.jsx
+++ src/pages/me/MeLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet, NavLink } from "react-router-dom";
 export default function MeLayout() {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">AZTUALIZADO 5</h1>
+      <h1 className="text-2xl font-bold mb-4">AZTUALIZADO 6</h1>
       
       <Outlet />
     </div>
