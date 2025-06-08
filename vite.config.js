d250a20ee739341798308d069b710e1fe5269d44
Kendercuba <kendercuba92@gmail.com>
2025-06-08 19:38:59 -0400

    💄 Cambios visuales en el frontend

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/', // 👈 Esto asegura que las rutas sean relativas al dominio raíz
   plugins: [react()],
 });
