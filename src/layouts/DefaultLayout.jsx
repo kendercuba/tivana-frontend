3a3962d17a6c2f79858ccd3b3773036aa2dcde74
Kendercuba <kendercuba92@gmail.com>
2025-07-13 17:19:44 -0400

    defaultlayout.jsx se corrigio el problema que en el modo movil no tenga un contenedor raiz y por eso aparezca un borde rojo arriba del header en el telefono

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -5,16 +5,14 @@ import { Outlet } from "react-router-dom";
 
 export default function DefaultLayout() {
   return (
-    <>
+    <div className="min-h-screen flex flex-col bg-white"> {/* ✅ Contenedor raíz */}
       <Header />
-      <main className="pt-0 mt-0 border-4 border-red-500 overflow-visible">
-
+      <main className="flex-grow pt-0 mt-0 border-4 border-red-500 overflow-visible">
         <div className="max-w-screen-2xl mx-auto px-4 border-4 border-blue-500 min-h-[1px]">
-
           <Outlet />
         </div>
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
